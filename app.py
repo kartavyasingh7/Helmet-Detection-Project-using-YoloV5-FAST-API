f6b84b366850c448d9eae25df0e1f3575c56f49a
kartavyasingh7 <kartavya.cr07@gmail.com>
2025-06-01 17:57:47 +0530

    third commit

--- app.py
+++ app.py
@@ -5,23 +5,27 @@ from PIL import Image
 import io
 
 app = FastAPI()
+model = None  
 
+@app.on_event("startup")
+def load_model():
+    global model
+    model = torch.hub.load(
+        'ultralytics/yolov5',
+        'custom',
+        path='C:/Users/Acer/Desktop/Project/yolov5/runs/train/exp6/weights/best.pt'
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:\Users\Acer\Desktop\Project\yolov5runs\train\exp6\weights\best.pt', force_reload=True)
+        force_reload=True
+    )
 
 @app.post("/detect")
 async def detect_helmet(file: UploadFile = File(...)):
-   
-    image_bytes = await file.read()
-    image = Image.open(io.BytesIO(image_bytes))
-
- 
-    results = model(image)
-
-
-    detections = results.pandas().xyxy[0].to_dict(orient="records")
-
-
-    filtered = [det for det in detections if det['confidence'] > 0.4]
-
-    return JSONResponse(content={"detections": filtered})
+    try:
+        image_bytes = await file.read()
+        image = Image.open(io.BytesIO(image_bytes))
+        results = model(image)
+        detections = results.pandas().xyxy[0].to_dict(orient="records")
+        filtered = [det for det in detections if det['confidence'] > 0.4]
+        return JSONResponse(content={"detections": filtered})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
