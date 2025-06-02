61f780416543a122a0085edad0cbba9329af62d9
kartavyasingh7 <42486223+kartavyasingh7@users.noreply.github.com>
2025-06-02 13:48:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 model = None  
 origins = [
-    "http://localhost:3000",  # React frontend running locally
-    # You can add more origins if you need
+    "http://localhost:3000",  
+    
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Allow requests from the frontend origin
+    allow_origins=origins,  
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"], 
+    allow_headers=["*"],  
 )
 
 
